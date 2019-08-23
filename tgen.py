2c36a9e3df5b2dde7a1133a2c1b80d85942f3dd6
nobody246 <a.se8seven@gmail.com>
2019-08-22 23:22:25 -0400

    Update tgen.py

--- tgen.py
+++ tgen.py
@@ -2,11 +2,11 @@ import pyaudio
 import numpy as np
 from time import sleep
 from cypher import readableToCypher
-
+import signal
 #todo add as params
 volume = 1
 fs = 88200
-duration = .25
+duration = .5 #can modify this value without changing rx code
 f = 0
 lo = 2200.0
 two = 2400.0
@@ -15,20 +15,22 @@ four = 2800.0
 five = 3000.0
 freqs = [lo, two, three, four,five]
 message="           THE_GRASS_IS_ALWAYS_GREENER_ON_THE_OTHER_SIDE_YANKEE_ALPHA_FOXTROT "
-
 p = pyaudio.PyAudio()
-
-def playFreq():
-   global f, fs, volume, sp, duration
-   samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
-   stream.write(volume * samples)
-
-   
 stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=fs,
                 output=True)
-
+def sigHandler(s, fr):
+   global p, stream
+   stream.stop_stream()
+   stream.close()
+   p.terminate()     
+   exit(0)
+signal.signal(signal.SIGINT, sigHandler)
+def playFreq():
+   global f, fs, volume, sp, duration, stream
+   samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+   stream.write(volume * samples)
 def prepareStr(st):
    r = ""
    for x in st:
@@ -42,6 +44,3 @@ while True:
     for m in prepareStr(message):
         f = freqs[int(m)]
         playFreq()
-stream.stop_stream()
-stream.close()
-p.terminate()
